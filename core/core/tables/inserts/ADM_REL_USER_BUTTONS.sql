ca6616a945d65838a6765045f31f259c43ef59ec
lxudayberdiyev <lxudayberdiev@sqb.uz>
2026-08-13 12:28:34 +0500

    -

---
+++ core/core/tables/inserts/ADM_REL_USER_BUTTONS.sql
@@ -0,0 +1,21 @@
+insert into ADM_REL_USER_BUTTONS (USER_ID, BUTTON_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 1, 3003, to_date('31-07-2026', 'dd-mm-yyyy'), to_date('31-07-2036', 'dd-mm-yyyy'), 'A', -1, to_date('03-08-2026 14:23:38', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('03-08-2026 14:23:38', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into ADM_REL_USER_BUTTONS (USER_ID, BUTTON_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 2, 3003, to_date('31-07-2026', 'dd-mm-yyyy'), to_date('31-07-2036', 'dd-mm-yyyy'), 'A', -1, to_date('03-08-2026 14:23:38', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('03-08-2026 14:23:38', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into ADM_REL_USER_BUTTONS (USER_ID, BUTTON_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 3, 3003, to_date('31-07-2026', 'dd-mm-yyyy'), to_date('31-07-2036', 'dd-mm-yyyy'), 'A', -1, to_date('03-08-2026 14:23:38', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('03-08-2026 14:23:38', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into ADM_REL_USER_BUTTONS (USER_ID, BUTTON_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 4, 3003, to_date('31-07-2026', 'dd-mm-yyyy'), to_date('31-07-2036', 'dd-mm-yyyy'), 'A', -1, to_date('03-08-2026 14:23:38', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('03-08-2026 14:23:38', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into ADM_REL_USER_BUTTONS (USER_ID, BUTTON_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 5, 3003, to_date('31-07-2026', 'dd-mm-yyyy'), to_date('31-07-2036', 'dd-mm-yyyy'), 'A', -1, to_date('03-08-2026 14:23:38', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('03-08-2026 14:23:38', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into ADM_REL_USER_BUTTONS (USER_ID, BUTTON_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 10, 8001, to_date('10-08-2026 12:13:18', 'dd-mm-yyyy hh24:mi:ss'), to_date('31-12-9999', 'dd-mm-yyyy'), 'A', -1, to_date('10-08-2026 12:13:18', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('10-08-2026 12:13:18', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into ADM_REL_USER_BUTTONS (USER_ID, BUTTON_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 11, 8001, to_date('10-08-2026 12:13:18', 'dd-mm-yyyy hh24:mi:ss'), to_date('31-12-9999', 'dd-mm-yyyy'), 'A', -1, to_date('10-08-2026 12:13:18', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('10-08-2026 12:13:18', 'dd-mm-yyyy hh24:mi:ss'));
+
---
+++ core/core/tables/inserts/ADM_REL_USER_MENUS.sql
@@ -0,0 +1,105 @@
+insert into ADM_REL_USER_MENUS (USER_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 1000, to_date('28-07-2026', 'dd-mm-yyyy'), to_date('31-12-9999', 'dd-mm-yyyy'), 'A', -1, to_date('29-07-2026 15:41:08', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('29-07-2026 15:41:08', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into ADM_REL_USER_MENUS (USER_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 2001, to_date('31-07-2026', 'dd-mm-yyyy'), to_date('31-07-2036', 'dd-mm-yyyy'), 'A', -1, to_date('29-07-2026 15:41:08', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('31-07-2026 11:10:31', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into ADM_REL_USER_MENUS (USER_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 2005, to_date('04-08-2026', 'dd-mm-yyyy'), to_date('31-12-2099', 'dd-mm-yyyy'), 'A', -1, to_date('04-08-2026', 'dd-mm-yyyy'), -1, to_date('04-08-2026', 'dd-mm-yyyy'));
+
+insert into ADM_REL_USER_MENUS (USER_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 2007, to_date('07-08-2026', 'dd-mm-yyyy'), to_date('31-12-9999', 'dd-mm-yyyy'), 'A', -1, to_date('07-08-2026', 'dd-mm-yyyy'), -1, to_date('07-08-2026', 'dd-mm-yyyy'));
+
+insert into ADM_REL_USER_MENUS (USER_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 3000, to_date('31-07-2026', 'dd-mm-yyyy'), to_date('31-07-2036', 'dd-mm-yyyy'), 'A', -1, to_date('29-07-2026 15:41:08', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('31-07-2026 11:10:31', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into ADM_REL_USER_MENUS (USER_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 3001, to_date('28-07-2026', 'dd-mm-yyyy'), to_date('31-12-9999', 'dd-mm-yyyy'), 'A', -1, to_date('29-07-2026 15:41:08', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('29-07-2026 15:41:08', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into ADM_REL_USER_MENUS (USER_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 3002, to_date('31-07-2026', 'dd-mm-yyyy'), to_date('31-07-2036', 'dd-mm-yyyy'), 'A', -1, to_date('29-07-2026 15:41:08', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('31-07-2026 11:10:31', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into ADM_REL_USER_MENUS (USER_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 3003, to_date('31-07-2026', 'dd-mm-yyyy'), to_date('31-07-2036', 'dd-mm-yyyy'), 'A', -1, to_date('29-07-2026 15:41:08', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('31-07-2026 11:10:31', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into ADM_REL_USER_MENUS (USER_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 3004, to_date('31-07-2026', 'dd-mm-yyyy'), to_date('31-07-2036', 'dd-mm-yyyy'), 'A', -1, to_date('29-07-2026 15:41:08', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('31-07-2026 11:10:31', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into ADM_REL_USER_MENUS (USER_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 3005, to_date('31-07-2026', 'dd-mm-yyyy'), to_date('31-07-2036', 'dd-mm-yyyy'), 'A', -1, to_date('03-08-2026 14:03:07', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('03-08-2026 14:03:07', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into ADM_REL_USER_MENUS (USER_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 3006, to_date('31-07-2026', 'dd-mm-yyyy'), to_date('31-07-2036', 'dd-mm-yyyy'), 'A', -1, to_date('03-08-2026 14:03:07', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('03-08-2026 14:03:07', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into ADM_REL_USER_MENUS (USER_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 3007, to_date('31-07-2026', 'dd-mm-yyyy'), to_date('31-07-2036', 'dd-mm-yyyy'), 'A', -1, to_date('03-08-2026 14:03:07', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('03-08-2026 14:03:07', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into ADM_REL_USER_MENUS (USER_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 3008, to_date('31-07-2026', 'dd-mm-yyyy'), to_date('31-07-2036', 'dd-mm-yyyy'), 'A', -1, to_date('03-08-2026 14:03:07', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('03-08-2026 14:03:07', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into ADM_REL_USER_MENUS (USER_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 3009, to_date('04-08-2026 15:46:33', 'dd-mm-yyyy hh24:mi:ss'), to_date('31-07-2036', 'dd-mm-yyyy'), 'A', -1, to_date('04-08-2026 15:46:33', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('04-08-2026 15:46:33', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into ADM_REL_USER_MENUS (USER_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 4000, to_date('28-07-2026', 'dd-mm-yyyy'), to_date('31-12-9999', 'dd-mm-yyyy'), 'A', -1, to_date('29-07-2026 15:41:08', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('29-07-2026 15:41:08', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into ADM_REL_USER_MENUS (USER_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 4001, to_date('28-07-2026', 'dd-mm-yyyy'), to_date('31-12-9999', 'dd-mm-yyyy'), 'A', -1, to_date('29-07-2026 15:41:08', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('29-07-2026 15:41:08', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into ADM_REL_USER_MENUS (USER_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 4002, to_date('31-07-2026', 'dd-mm-yyyy'), to_date('31-07-2036', 'dd-mm-yyyy'), 'A', -1, to_date('29-07-2026 15:41:08', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('31-07-2026 11:10:31', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into ADM_REL_USER_MENUS (USER_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 4003, to_date('31-07-2026', 'dd-mm-yyyy'), to_date('31-07-2036', 'dd-mm-yyyy'), 'A', -1, to_date('29-07-2026 15:41:08', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('31-07-2026 11:10:31', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into ADM_REL_USER_MENUS (USER_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 4004, to_date('31-07-2026', 'dd-mm-yyyy'), to_date('31-07-2036', 'dd-mm-yyyy'), 'A', -1, to_date('29-07-2026 15:41:08', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('31-07-2026 11:10:31', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into ADM_REL_USER_MENUS (USER_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 4005, to_date('31-07-2026', 'dd-mm-yyyy'), to_date('31-07-2036', 'dd-mm-yyyy'), 'A', -1, to_date('29-07-2026 15:41:08', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('31-07-2026 11:10:31', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into ADM_REL_USER_MENUS (USER_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 4006, to_date('31-07-2026', 'dd-mm-yyyy'), to_date('31-07-2036', 'dd-mm-yyyy'), 'A', -1, to_date('29-07-2026 15:41:08', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('31-07-2026 11:10:31', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into ADM_REL_USER_MENUS (USER_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 5000, to_date('28-07-2026', 'dd-mm-yyyy'), to_date('31-12-9999', 'dd-mm-yyyy'), 'A', -1, to_date('29-07-2026 15:41:08', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('29-07-2026 15:41:08', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into ADM_REL_USER_MENUS (USER_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 5001, to_date('31-07-2026', 'dd-mm-yyyy'), to_date('31-07-2036', 'dd-mm-yyyy'), 'A', -1, to_date('29-07-2026 15:41:08', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('31-07-2026 11:10:31', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into ADM_REL_USER_MENUS (USER_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 5002, to_date('31-07-2026', 'dd-mm-yyyy'), to_date('31-07-2036', 'dd-mm-yyyy'), 'A', -1, to_date('29-07-2026 15:41:08', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('31-07-2026 11:10:31', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into ADM_REL_USER_MENUS (USER_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 5003, to_date('31-07-2026', 'dd-mm-yyyy'), to_date('31-07-2036', 'dd-mm-yyyy'), 'A', -1, to_date('29-07-2026 15:41:08', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('31-07-2026 11:10:31', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into ADM_REL_USER_MENUS (USER_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 5004, to_date('31-07-2026', 'dd-mm-yyyy'), to_date('31-07-2036', 'dd-mm-yyyy'), 'A', -1, to_date('29-07-2026 15:41:08', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('31-07-2026 11:10:31', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into ADM_REL_USER_MENUS (USER_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 5005, to_date('31-07-2026', 'dd-mm-yyyy'), to_date('31-07-2036', 'dd-mm-yyyy'), 'A', -1, to_date('29-07-2026 15:41:08', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('31-07-2026 11:10:31', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into ADM_REL_USER_MENUS (USER_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 6000, to_date('28-07-2026', 'dd-mm-yyyy'), to_date('31-12-9999', 'dd-mm-yyyy'), 'A', -1, to_date('29-07-2026 15:41:08', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('29-07-2026 15:41:08', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into ADM_REL_USER_MENUS (USER_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 6001, to_date('31-07-2026', 'dd-mm-yyyy'), to_date('31-07-2036', 'dd-mm-yyyy'), 'A', -1, to_date('29-07-2026 15:41:08', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('31-07-2026 11:10:31', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into ADM_REL_USER_MENUS (USER_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 6002, to_date('31-07-2026', 'dd-mm-yyyy'), to_date('31-07-2036', 'dd-mm-yyyy'), 'A', -1, to_date('29-07-2026 15:41:08', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('31-07-2026 11:10:31', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into ADM_REL_USER_MENUS (USER_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 7000, to_date('29-07-2026', 'dd-mm-yyyy'), to_date('31-12-2099', 'dd-mm-yyyy'), 'A', -1, to_date('29-07-2026 15:59:52', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('29-07-2026 15:59:52', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into ADM_REL_USER_MENUS (USER_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 7001, to_date('29-07-2026', 'dd-mm-yyyy'), to_date('31-12-2099', 'dd-mm-yyyy'), 'A', -1, to_date('29-07-2026 15:59:52', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('29-07-2026 15:59:52', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into ADM_REL_USER_MENUS (USER_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 7002, to_date('03-08-2026', 'dd-mm-yyyy'), to_date('31-12-9999', 'dd-mm-yyyy'), 'A', -1, to_date('03-08-2026 15:43:35', 'dd-mm-yyyy hh24:mi:ss'), -1, to_date('03-08-2026 15:43:35', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into ADM_REL_USER_MENUS (USER_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 8000, to_date('03-08-2026', 'dd-mm-yyyy'), to_date('31-12-9999', 'dd-mm-yyyy'), 'A', -1, to_date('03-08-2026', 'dd-mm-yyyy'), -1, to_date('03-08-2026', 'dd-mm-yyyy'));
+
+insert into ADM_REL_USER_MENUS (USER_ID, MENU_ID, DATE_ACTIVATE, DATE_DEACTIVATE, STATE, CREATED_BY, CREATED_ON, MODIFY_BY, MODIFY_ON)
+values (-1, 8001, to_date('03-08-2026', 'dd-mm-yyyy'), to_date('31-12-9999', 'dd-mm-yyyy'), 'A', -1, to_date('03-08-2026', 'dd-mm-yyyy'), -1, to_date('03-08-2026', 'dd-mm-yyyy'));
+
